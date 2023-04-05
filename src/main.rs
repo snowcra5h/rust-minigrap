f326b7272941933d63c7bf2fc47e05d38182b6ad
Michael Walker <snowcra5h@icloud.com>
2023-04-05 00:48:22 -0600

    modularize minigrep

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,9 @@
 mod globals;
 use globals::{SEARCH_STR, QUERY_STR};
 
-use std::{env, fs, process, error::Error};
+use minigrep::Config;
+
+use std::{env, process};
 
 fn main() {
     let args: Vec<String>  = env::args().collect();
@@ -18,35 +20,10 @@ fn main() {
     println!("\n{SEARCH_STR:20} : {:20}", config.query);
     println!("{QUERY_STR:20} : {:20}", config.file_path);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
     }
 }
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("\nWith text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query: String = args[1].clone();
-        let file_path: String = args[2].clone();
-
-        Ok(Config { query, file_path })
-    }
-}
 
