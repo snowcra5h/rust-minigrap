900c5c0b6b0a44500390e74733ba3c2846f3bb84
Michael Walker <snowcra5h@icloud.com>
2023-04-04 23:39:56 -0600

    reformat argv parser

--- src/main.rs
+++ src/main.rs
@@ -8,17 +8,30 @@ use globals::{SEARCH_STR, QUERY_STR};
 use std::{env, fs};
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<String>  = env::args().collect();
 
-    let query:     &String = &args[1];
-    let file_path: &String = &args[2];
+    let config: Config = Config::new(&args);
 
-    println!("\n{SEARCH_STR:20} : {:20}", query);
-    println!("{QUERY_STR:20} : {:20}", file_path);
+    println!("\n{SEARCH_STR:20} : {:20}", config.query);
+    println!("{QUERY_STR:20} : {:20}", config.file_path);
 
-    let contents = fs::read_to_string(file_path)
+    let contents = fs::read_to_string(config.file_path)
         .expect("Should have been able to read the file");
 
     println!("\nWith text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
 
